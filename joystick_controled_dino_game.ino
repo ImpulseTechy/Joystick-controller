acd9d69e02018e176cbf9d8590bb4fbea1f2b3fb
ImpulseTech <132160028+ImpulseTechy@users.noreply.github.com>
2023-11-05 13:24:40 +0530

    Update joystick_controled_dino_game.ino

--- joystick_controled_dino_game.ino
+++ joystick_controled_dino_game.ino
@@ -1,12 +1,22 @@
 void setup() {
-  Serial.begin(9600);
+    Serial.begin(9600);
 }
 
 void loop() {
-  int xValue = analogRead(A0);  // Use the appropriate pin for the X-axis
-  if (xValue < 100) {
-    // Trigger the jump action by sending 'J' character
-    Serial.write('J');
-    delay(100); // Adjust the delay as needed
-  }
+    int xValue = analogRead(A0); // Use the appropriate pin for the X-axis
+    int yValue = analogRead(A1); // Use the appropriate pin for the Y-axis
+
+    if (xValue < 100) {
+        Serial.write('L'); // 'L' for left
+        delay(100); // Adjust the delay as needed
+    } else if (xValue > 900) {
+        Serial.write('R'); // 'R' for right
+        delay(100); // Adjust the delay as needed
+    } else if (yValue < 100) {
+        Serial.write('U'); // 'U' for up (jump)
+        delay(100); // Adjust the delay as needed
+    } else if (yValue > 900) {
+        Serial.write('D'); // 'D' for down
+        delay(100); // Adjust the delay as needed
+    }
 }
